18be25d0e85a180ac96335e37d75315c360a5004
Kloym <oleg.petr1223@gmail.com>
2025-04-02 09:56:20 +0300

    f

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 pip3 install gunicorn
-make install && psql -a -d $DATABASE -f database.sql
+make install && psql -a -d $DATABASE_URL -f database.sql
